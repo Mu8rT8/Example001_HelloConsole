6b51bd6b4772ae98bf042b047e09dc4c165bd9b2
Maksim Kamarou <git.rep@mail.ru>
2023-02-09 12:21:03 +0100

    Example 005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ввидите имя пользователя: ");
+string username = Console.ReadLine();
+
+if( username.ToLower() == "максим")
+{
+    Console.WriteLine("Ура, это же МАКСИМ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
